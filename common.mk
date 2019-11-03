8c6d1e091d5fcf6546bffbda82db297ccbaba0a8
Bauuuuu <abansode@outlook.com>
2019-11-03 10:31:25 +0000

    Revert "msm8998-common: Switch to 1.2 version of qcom power HAL"
    This reverts commit 4b65cf2f1ee81d93b61568c22890950484263c32.
    
    Change-Id: I43c5230a644c2afb1ccb9e90f35be0daa57fcc32

--- common.mk
+++ common.mk
@@ -367,7 +367,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
