3fa7dbfb61996723cf56a734998a133ffb871c9a
Pranav Vashi <neobuddy89@gmail.com>
2020-03-22 20:42:16 +0530

    msm8998-common: Nuke camera shims
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -151,7 +151,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    libshim_camera \
     Camera2
 
 PRODUCT_PACKAGES += \
