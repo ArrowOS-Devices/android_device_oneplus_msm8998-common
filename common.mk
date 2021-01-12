092a755ec1c651c740a96729f0ae1e175c0af8cc
Bauuuuu <abansode@outlook.com>
2021-01-12 14:59:00 +0100

    msm8998-common: include carrierconfigs overlays from vendor/arrow

--- common.mk
+++ common.mk
@@ -31,8 +31,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
+    $(LOCAL_PATH)/overlay-lineage \
+    vendor/arrow/overlay/CarrierConfig
+    
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
