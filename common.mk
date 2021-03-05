0d0ef9cba3127310f48298c105d0b7e4adabe66f
Timi <timi.rautamaki@gmail.com>
2021-03-05 16:21:56 +0100

    [DNM]msm8998-common: don't inherit from oppo-common  tree

--- common.mk
+++ common.mk
@@ -504,9 +504,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
-
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0 \
